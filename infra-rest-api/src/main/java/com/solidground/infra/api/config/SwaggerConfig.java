80b1c90cc108a874e0fb8b080f52b288d25a7595
Furkan Kürşat Danışmaz <danismaz.furkan@gmail.com>
2019-01-03 10:33:09 +0300

    Swagger Config Improved

--- infra-rest-api/src/main/java/com/solidground/infra/api/config/SwaggerConfig.java
+++ infra-rest-api/src/main/java/com/solidground/infra/api/config/SwaggerConfig.java
@@ -5,9 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
@@ -26,13 +30,60 @@ public class SwaggerConfig {
 	@Value("${swagger.api.base}")
 	private String swaggerApiBase;
 
+	@Value("${swagger.api.info.enabled}")
+	private boolean swaggerApiInfoEnabled;
+
+	@Value("${swagger.api.info.title}")
+	private String title;
+
+	@Value("${swagger.api.info.description}")
+	private String description;
+
+	@Value("${swagger.api.info.version}")
+	private String version;
+
+	@Value("${swagger.api.info.terms_of_service_url}")
+	private String termsOfServiceUrl;
+
+	@Value("${swagger.api.info.contact.name}")
+	private String contactName;
+
+	@Value("${swagger.api.info.contact.url}")
+	private String contactUrl;
+
+	@Value("${swagger.api.info.contact.email}")
+	private String contactEmail;
+
+	@Value("${swagger.api.info.license.name}")
+	private String licenseName;
+
+	@Value("${swagger.api.info.license.url}")
+	private String licenseUrl;
+
 	@Bean
 	public Docket producApi() {
-		return new Docket(DocumentationType.SWAGGER_2)
+		Docket productApi = new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage(this.swaggerDocumentationBase))
 				.paths(regex(this.swaggerApiBase))
 				.build();
 
+		if (this.swaggerApiInfoEnabled) {
+			productApi = productApi.apiInfo(this.metaInfo());
+		}
+
+		return productApi;
+	}
+
+	@Bean
+	public ApiInfo metaInfo() {
+		return new ApiInfo(
+				this.title,
+				this.description,
+				this.version,
+				this.termsOfServiceUrl,
+				new Contact(this.contactName, this.contactUrl, this.contactEmail),
+				this.licenseName,
+				this.licenseUrl, Collections.emptyList());
 	}
 }
